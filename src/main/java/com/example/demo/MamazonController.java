a041feb30bd39f7e22937671a6fd664ae2ff8922
umetsu <umetsu.vista@gmail.com>
2017-10-14 21:49:16 +0900

    merge

--- src/main/java/com/example/demo/MamazonController.java
+++ src/main/java/com/example/demo/MamazonController.java
@@ -2,14 +2,9 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/hoge")
@@ -18,7 +13,7 @@ public class MamazonController {
     AccountService service;
 
     @RequestMapping(value="/",method=RequestMethod.GET)
-    public void index(){
+    public void index(Model model){
 
         // レコードを取得してページのmodelにセットしておく
         Account account = service.findById(1l);
