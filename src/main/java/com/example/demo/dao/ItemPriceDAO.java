b98b0f300b5541701af58113d214c522052275d5
山口潤 <yamaguchijun@yamaguchijun-no-MacBook-Pro-3.local>
2017-10-22 21:35:11 +0900

    DAO

---
+++ src/main/java/com/example/demo/dao/ItemPriceDAO.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.ItemPrice;
+
+@Repository
+public interface ItemPriceDAO extends JpaRepository<ItemPrice, Long>{
+
+}
