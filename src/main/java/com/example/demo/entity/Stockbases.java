4726d3719ddbcc4defd9ecb10fe742c5069d308f
YAMAGUCHIJUN <mrchildren1202@gmail.com>
2017-11-05 20:30:13 +0900

    アノテーション

--- src/main/java/com/example/demo/entity/Stockbases.java
+++ src/main/java/com/example/demo/entity/Stockbases.java
@@ -2,15 +2,16 @@ package com.example.demo.entity;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="stock_bases")
 @Data
 public class Stockbases {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @SequenceGenerator(name = "stock_bases_id_seq", sequenceName = "stock_bases_id_seq", allocationSize = 1)
   @Column(name="id")
   private Long id;
 
